dc46adb0c63a3523713ece7a2b3e794b4c79c312
Seth Grotelueschen <sgrotelueschen@westmont.edu>
2013-08-13 18:45:58 -0700

    full functionality

--- public/js/application.js
+++ public/js/application.js
@@ -1,3 +1,13 @@
 $(document).ready(function(){
+  $('#get_color').on('click', function(e){
+    e.preventDefault();
+    $.ajax({
+      type: 'post',
+      url: this.href,
+      dataType: "JSON"
 
+    }).done(function(server_data) {
+    $("#color_me li:nth-child("+server_data.cell+")").css('background-color', server_data.color);
+    });
+  });
 });
